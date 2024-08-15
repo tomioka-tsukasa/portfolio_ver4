354d66e2e4c11fe3c7f35cfd3e332796f4047be2
Tomioka_Tsukasa <tomioka.tsukasa@holon-inc.jp>
2024-08-15 09:34:22 +0900

    learning: css-modules: ファイル、ディレクトリ調整

---
+++ src/app/learning/css-modules/components/bye/bye.tsx
@@ -0,0 +1,9 @@
+import styles from './bye.module.scss'
+
+export default function Bye() {
+  return <>
+    <h1 className={styles.hello}>
+      Bye
+    </h1>
+  </>
+}
--- src/app/learning/css-modules/page.tsx
+++ src/app/learning/css-modules/page.tsx
@@ -1,7 +1,9 @@
-import Hello from "./components/hello";
+import Bye from "./components/bye/bye";
+import Hello from "./components/hello/hello";
 
 export default function StyledComponents() {
   return <>
     <Hello />
+    <Bye />
   </>
 }
