16e819f1119f59afe7dd77ebf581c1c6fffed5cb
Tomioka_Tsukasa <tomioka.tsukasa@holon-inc.jp>
2024-08-18 04:18:17 +0900

    add: scssカラー定義

---
+++ src/lib/fonts/index.ts
@@ -0,0 +1,31 @@
+import { Roboto, Zen_Old_Mincho } from "next/font/google";
+
+export const zenOldMincho_w400 = Zen_Old_Mincho({ 
+  subsets: ["latin"],
+  weight: '400'
+})
+
+export const zenOldMincho_w500 = Zen_Old_Mincho({ 
+  subsets: ["latin"],
+  weight: '500'
+})
+
+export const zenOldMincho_w600 = Zen_Old_Mincho({ 
+  subsets: ["latin"],
+  weight: '600'
+})
+
+export const zenOldMincho_w700 = Zen_Old_Mincho({ 
+  subsets: ["latin"],
+  weight: '700'
+})
+
+export const zenOldMincho_w900 = Zen_Old_Mincho({ 
+  subsets: ["latin"],
+  weight: '900'
+})
+
+export const roboto = Roboto({ 
+  subsets: ["latin"],
+  weight: '400'
+})
