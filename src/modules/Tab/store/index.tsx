51267008c1812e1462ea35478a3d906db5091453
Tomioka_Tsukasa <tomioka.tsukasa@holon-inc.jp>
2024-08-25 00:51:07 +0900

    refactor: Tabを独立して扱えるようにリファクタリング、PJ共通で扱うモジュールとして配下に移動

---
+++ src/modules/Tab/store/index.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, Dispatch, useContext, useReducer } from "react";
+import { ActionPayload, InitialState, Initializer, ProviderProps } from "../types";
+
+const reducer = (state: InitialState, action: ActionPayload) => {
+  return {
+    ...state,
+    active: action.active,
+  }
+}
+
+const initializer: Initializer = (initialState) => ({
+  ...initialState
+})
+
+const StateContext = createContext<InitialState | undefined>(undefined)
+
+const ChangeContext = createContext<Dispatch<ActionPayload> | undefined>(undefined)
+
+export default function TabProvider({
+  children,
+  initialState
+}: ProviderProps ) {
+  const [state, setActive] = useReducer(reducer, initialState, initializer)
+  return <>
+    <StateContext.Provider value={state}>
+      <ChangeContext.Provider value={setActive}>
+        {children}
+      </ChangeContext.Provider>
+    </StateContext.Provider>
+  </>
+}
+
+export const useStateContext = () => {
+  const context = useContext(StateContext)
+  if (context === undefined) throw new Error("useStateContext must be used within a TabProvider")
+  return context
+}
+
+export const useChangeContext = () => {
+  const context = useContext(ChangeContext)
+  if (context === undefined) throw new Error("ChangeContext has not been provided.")
+  return context
+}
