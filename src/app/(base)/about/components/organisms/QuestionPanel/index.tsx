9037adeeb7a5160a46feeb7573f767c2de60e730
tomioka-tsukasa <tsukasa.creator@gmail.com>
2024-09-02 02:33:31 +0900

    add: スタイル追加

--- src/app/(base)/about/components/organisms/QuestionPanel/index.tsx
+++ src/app/(base)/about/components/organisms/QuestionPanel/index.tsx
@@ -6,6 +6,7 @@ import { getContentsManagerBySlug } from "@/lib/newt";
 import QaCategory from "../../atoms/QaCategory";
 import QuestionList from "../../molecules/QuestionList";
 import TabContent from "@/modules/Tab/components/TabContent";
+import TerminalPanel from "@/components/organisms/TerminalPanel";
 
 export default async function QuestionPanel() {
   const contents = await getContentsManagerBySlug('portfolio-talkme');
@@ -17,14 +18,23 @@ export default async function QuestionPanel() {
   return <>
     <div className={styles.root}>
       <TabProvider initialState={{active: 'myself'}}>
-        <QaCategoryList list={tabList} />
-        {tabList.map( member => (
-          <div className={styles.contents}>
-            <TabContent member={member} key={member.id}>
-              <QuestionList items={contents['qa-item-field']} category={member.id} />
-            </TabContent>
+        <TerminalPanel title={'- Question to Tsukasa Tomioka. -'} annotation={'⌥⌘1'}>
+          <div className={styles.wrapper}>
+            <p className={styles.console}>
+              tsukasa.tomioka.@portfolio ~ % selected question category: “Who Are You”, Hit 14 questions. ｜
+            </p>
+            <div className={styles.categorys}>
+              <QaCategoryList list={tabList} />
+            </div>
+            {tabList.map( member => (
+              <div className={styles.contents}>
+                <TabContent member={member} key={member.id}>
+                  <QuestionList items={contents['qa-item-field']} category={member.id} />
+                </TabContent>
+              </div>
+            ))}
           </div>
-        ))}
+        </TerminalPanel>
       </TabProvider>
     </div>
   </>
