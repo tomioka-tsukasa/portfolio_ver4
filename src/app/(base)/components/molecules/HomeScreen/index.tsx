f3198524ec5b122303c9efbae9943a4fe7a3ff28
tomioka-tsukasa <tsukasa.creator@gmail.com>
2024-09-11 01:19:18 +0900

    add: HOMEアニメーション実装(副作用有り)

--- src/app/(base)/components/molecules/HomeScreen/index.tsx
+++ src/app/(base)/components/molecules/HomeScreen/index.tsx
@@ -3,16 +3,23 @@
 import { zenOldMincho_w400 } from "@/lib/fonts";
 import styles from "./_index.module.scss";
 import Typing from "@/components/atoms/Typing";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function HomeScreen() {
-  const [status, setStatus] = useState(true)
+  const [status, setStatus] = useState(false)
+  useEffect(() => {
+    document.body.classList.add('homeAnimationInit')
+    setTimeout(() => {
+      setStatus(true)
+    }, 700);
+  }, [status, setStatus])
   return <>
     <div className={styles.root}>
-      <h1 className={`${styles.head} ${zenOldMincho_w400.className}`}>
+      <h1 className={`${styles.head} ${zenOldMincho_w400.className} ${status ? styles.isActive : ''}`}>
         <Typing 
           text={'Hello Tsukasa Tomioka Portfolio. ver4.0'} 
           speed={1} 
+          trigger={status}
           initDisplay={false} 
         />
       </h1>
