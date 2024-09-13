9a904f5e2a0308e32d892191408519eb7d251c51
tomioka-tsukasa <tsukasa.creator@gmail.com>
2024-09-13 20:55:06 +0900

    fix: (uni-lab)と(base)のレイアウトを共通化

--- src/app/(uni-lab)/layout.tsx
+++ src/app/(uni-lab)/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import "@/sass/globals.scss";
-import StoreProvider from "@/lib/store/provider";
 import styles from "./_layout.module.scss";
-import { zenKakuGothicNew_w400 } from "@/lib/fonts";
 import Footer from "@/components/organisms/Footer";
 import Header from "@/app/(uni-lab)/components/organisms/Header";
-import NavigatingType from "../(base)/components/atoms/NavigatingType";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,21 +14,15 @@ export default function UniLabLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja" className={`${styles.html}`}>
-      <StoreProvider>
-        <body className={`${styles.body} ${zenKakuGothicNew_w400.className}`}>
-          <NavigatingType />
-          <Header />
-          <div className={`${styles.container}`}>
-            {children}
-          </div>
-          <div className={styles.footer}>
-            <Footer />
-          </div>
-          <div className={`${styles.globalBg}`}></div>
-        </body>
-      </StoreProvider>
-    </html>
-  )
+  return <>
+    <div className={`${styles.root}`}>
+      <Header />
+      <div className={`${styles.container}`}>
+        {children}
+      </div>
+      <div className={styles.footer}>
+        <Footer />
+      </div>
+    </div>
+  </>
 }
