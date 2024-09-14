2c5ea7bf79770dd399845e1f2c244e91dc9dafd0
tomioka-tsukasa <tsukasa.creator@gmail.com>
2024-09-14 18:46:11 +0900

    add: LabNavコンポーネント分割

--- src/app/(root)/(uni-lab)/uni-lab/page.tsx
+++ src/app/(root)/(uni-lab)/uni-lab/page.tsx
@@ -1,15 +1,10 @@
 import styles from "./_index.module.scss"
-import LabSubjectList from "@/components/organisms/LabSubjectList";
-import { getLabGroup, getLabSubjects } from "@/lib/newt";
 import Image from "next/image";
 import logo from "/public/logo/logo-unilab.png"
 import { zenOldMincho_w700 } from "@/lib/fonts";
-import Heading from "@/components/atoms/Heading";
-import Button from "@/components/atoms/Button";
+import LabNav from "../components/organisms/LabNav/_index";
 
-export default async function ULHome() {
-  const subjects = await getLabSubjects()
-  const labGroups = await getLabGroup()
+export default function ULHome() {
   return <>
     <main className={styles.root}>
       <div className={styles.mv}>
@@ -22,32 +17,7 @@ export default async function ULHome() {
           Uni Lab.｜ハイエンドWeb制作研究所【開発途中！】
         </h1> 
       </div>
-      {labGroups.map( (group, index) => {
-        if (index >= 3) return 
-        return <>
-          <div className={`${styles.group} ${styles[group.slug]}`}>
-            <Heading type="border">
-              {group.title}
-            </Heading>
-            <div className={styles.list}>
-              <LabSubjectList subjects={subjects} scope={group} />
-            </div>
-            <div className={styles.groupEnter}>
-              <Button href={`/uni-lab/lab-group/${group.slug}/`}>
-                {`全ての「${group.title}」の研究項目を見る`}
-              </Button>
-            </div>
-          </div>
-        </>
-      })}
-      <div className={`${styles.group} ${styles.noCategory}`}>
-        <Heading type="border">
-          {'その他の研究項目'}
-        </Heading>
-        <div className={styles.list}>
-          <LabSubjectList subjects={subjects} scope={'noGroup'} />
-        </div>
-      </div>
+      <LabNav />
     </main>
   </>
 }
