bd3e42163984d854fb1bd77323cccda65c4ab8f4
tomioka-tsukasa <tsukasa.creator@gmail.com>
2024-09-15 12:56:27 +0900

    add: Newtの概要説明文に合わせて表示を調整

--- src/app/lab/react/react-module-tab-e3uKpKqxgo/page.tsx
+++ src/app/lab/react/react-module-tab-e3uKpKqxgo/page.tsx
@@ -1,94 +1,77 @@
 import styles from "./_index.module.scss"
-import TabContentBody from "@/app/(root)/(uni-lab)/components/molecules/TabContentBody"
-import TabList from "@/app/(root)/(uni-lab)/components/molecules/TabList"
 import Heading from "@/components/atoms/Heading"
 import TabContent from "@/modules/Tab/components/TabContent"
 import TabProvider from "@/modules/Tab/components/TabProvider"
-import { TabMember } from "@/modules/Tab/types"
+import TabTrigger from "@/modules/Tab/components/TabTrigger"
+import Connection from "./sections/Connection"
+import OtherTab from "./sections/OtherTab"
 
 export default function LabReactModuleTab() {
-  const list: Array<TabMember> = [
-    {
-      id: 'body',
-      name: '概要',
-      content: <TabContentBody body={'概要文が入ります。概要文が入ります。概要文が入ります。概要文が入ります。概要文が入ります。'} />
-    },
-    {
-      id: 'dev',
-      name: 'メモ',
-      content: <TabContentBody body={'メモが入ります。メモが入ります。メモが入ります。メモが入ります。メモが入ります。'} />
-    },
-    {
-      id: 'design',
-      name: 'トーク',
-      content: <TabContentBody body={'トークが入ります。トークが入ります。トークが入ります。トークが入ります。トークが入ります。'} />
-    },
-  ]
-  const dummy: Array<TabMember> = [
-    {
-      id: 'body',
-      name: '概要【Dummy】',
-      content: <TabContentBody body={'概要文【Dummy】が入ります。概要文【Dummy】が入ります。概要文【Dummy】が入ります。概要文【Dummy】が入ります。概要文【Dummy】が入ります。'} />
-    },
-    {
-      id: 'dev',
-      name: 'メモ【Dummy】',
-      content: <TabContentBody body={'メモ【Dummy】が入ります。メモ【Dummy】が入ります。メモ【Dummy】が入ります。メモ【Dummy】が入ります。メモ【Dummy】が入ります。'} />
-    },
-    {
-      id: 'design',
-      name: 'トーク【Dummy】',
-      content: <TabContentBody body={'トーク【Dummy】が入ります。トーク【Dummy】が入ります。トーク【Dummy】が入ります。トーク【Dummy】が入ります。トーク【Dummy】が入ります。'} />
-    },
-  ]
   return <>
     <article className={styles.root}>
       <div className={styles.heading}>
         <Heading tag="h3" type={'border'}>
-          {'タグが複数あっても連携可能'}
+          {'最小限の実装'}
         </Heading>
       </div>
       <div className={styles.tabArea}>
         <TabProvider initialState={{
-          active: list[0].id
+          active: '001'
         }}>
-          <TabList list={list} />
-          <div className={styles.wrapper}>
-            {list.map( member => (
-              <TabContent member={member} key={member.id}>
-                {member.content}
-              </TabContent>
-            ))}
+          <div className="list">
+            <TabTrigger member={{
+              id: '001'
+            }}>
+              <div style={{border: '1px solid #fff', padding: '9px'}}>
+                タブ001
+              </div>
+            </TabTrigger>
+            <TabTrigger member={{
+              id: '002'
+            }}>
+              <div style={{border: '1px solid #fff', padding: '9px'}}>
+                タブ002
+              </div>
+            </TabTrigger>
+            <TabTrigger member={{
+              id: '003'
+            }}>
+              <div style={{border: '1px solid #fff', padding: '9px'}}>
+                タブ003
+              </div>
+            </TabTrigger>
           </div>
-          <div className={styles.wrapper}>
-            {list.map( member => (
-              <TabContent member={member} key={member.id}>
-                {member.content}
-              </TabContent>
-            ))}
-          </div>
-          <TabList list={list} />
+          <TabContent member={{
+            id: '001'
+          }}>
+            <div style={{backgroundColor: 'rgba(255, 255, 255, .2)', padding: '9px'}}>
+              <p>
+                001
+              </p>
+            </div>
+          </TabContent>
+          <TabContent member={{
+            id: '002'
+          }}>
+            <div style={{backgroundColor: 'rgba(255, 255, 255, .2)', padding: '9px'}}>
+              <p>
+                002
+              </p>
+            </div>
+          </TabContent>
+          <TabContent member={{
+            id: '003'
+          }}>
+            <div style={{backgroundColor: 'rgba(255, 255, 255, .2)', padding: '9px'}}>
+              <p>
+                003
+              </p>
+            </div>
+          </TabContent>
         </TabProvider>
       </div>
-      <div className={styles.heading}>
-        <Heading tag="h3" type={'border'}>
-          {'Providerで囲んだ範囲しか影響しない'}
-        </Heading>
-      </div>
-      <div className={styles.tabArea}>
-        <TabProvider initialState={{
-          active: list[0].id
-        }}>
-          <TabList list={dummy} />
-          <div className={styles.wrapper}>
-            {dummy.map( member => (
-              <TabContent member={member} key={member.id}>
-                {member.content}
-              </TabContent>
-            ))}
-          </div>
-        </TabProvider>
-      </div>
+      <Connection />
+      <OtherTab />
     </article>
   </>
 }
