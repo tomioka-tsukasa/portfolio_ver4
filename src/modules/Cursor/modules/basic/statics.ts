533c0bc72683a9b8debad2632f68daa5b81268d9
tomioka-tsukasa <tsukasa.creator@gmail.com>
2024-09-22 23:47:20 +0900

    fix: setPointerの位置計算を絶対値から中央からの%に変更

--- src/modules/Cursor/modules/basic/statics.ts
+++ src/modules/Cursor/modules/basic/statics.ts
@@ -4,8 +4,8 @@ const setTarget = (
   target = {x: 0, y: 0},
 ) => {
   const handler: (event: PointerEvent) => void = (event) => {
-    target.x = Math.floor(event.clientX)
-    target.y = Math.floor(event.clientY)
+    target.x = Math.floor(((event.clientX / document.body.clientWidth) - 0.5) * 2 * 100)
+    target.y = Math.floor(((event.clientY / document.body.clientHeight) - 0.5) * 2 * 100)
   }
   window.addEventListener('pointermove', handler)
 }
@@ -21,10 +21,10 @@ const setCurrent = (
   const duration = body.duration ?? 1
   const gap = body.gap ?? 1
   const tick: () => number = () => {
-    pos.current.x += (pos.target.x - pos.current.x) * duration * gap
-    pos.current.y += (pos.target.y - pos.current.y) * duration * gap
-    const top = `${Math.floor(pos.current.y) - node.clientHeight / 2}px`
-    const left = `${Math.floor(pos.current.x) - node.clientWidth / 2}px`
+    pos.current.x += (pos.target.x - pos.current.x) * duration
+    pos.current.y += (pos.target.y - pos.current.y) * duration
+    const top = `calc(${50 + (0.5 * pos.current.y * gap)}% - ${node.clientHeight / 2}px)`
+    const left = `calc(${50 + (0.5 * pos.current.x * gap)}% - ${node.clientWidth / 2}px)`
     node.style.top = top
     node.style.left = left
     requestAnimationFrame(tick)
