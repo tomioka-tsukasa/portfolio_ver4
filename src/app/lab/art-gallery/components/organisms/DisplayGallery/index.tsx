8f9ebfe8842f003879fc278761dfff305016a5bc
tomioka-tsukasa <tsukasa.creator@gmail.com>
2024-10-04 04:32:12 +0900

    add: Header等の共通コンポーネントとDisplayGalleryの静的実装

---
+++ src/app/lab/art-gallery/components/organisms/DisplayGallery/index.tsx
@@ -0,0 +1,22 @@
+import { ArtSampleModels } from "../../../data/art-sample-models"
+import ArtModel from "../../molecules/ArtModel"
+import styles from "./_index.module.scss"
+
+type Props = {
+}
+
+export default function DisplayGallery({
+}: Props ) {
+  return <>
+    <div className={`${styles.root}`}>
+      <div className={`${styles.content}`}>
+        {ArtSampleModels.map( artModel => <div 
+          key={artModel.slug}
+          className={styles.artModel}
+        >
+          <ArtModel artModel={artModel} />
+        </div>)}
+      </div>
+    </div>
+  </>
+}
---
+++ src/app/lab/art-gallery/data/art-sample-models.ts
@@ -0,0 +1,84 @@
+import sampleVisual from "¥/public/lab/art-gallery/art-001.jpg"
+
+export const ArtSampleModels: Array<Cms.ArtModel> = [
+  {
+    slug: 'art-001',
+    title: 'Fabrica. Exp - 01',
+    enTitle: '',
+    author: 'Tsukasa Tomioka.',
+    visual: {
+      ...sampleVisual,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-002',
+    title: 'Fabrica. Exp - 02',
+    enTitle: '',
+    author: 'Tsukasa Tomioka.',
+    visual: {
+      ...sampleVisual,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-003',
+    title: 'Fabrica. Exp - 03',
+    enTitle: '',
+    author: 'Tsukasa Tomioka.',
+    visual: {
+      ...sampleVisual,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-004',
+    title: 'Fabrica. Exp - 04',
+    enTitle: '',
+    author: 'Tsukasa Tomioka.',
+    visual: {
+      ...sampleVisual,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-005',
+    title: 'Fabrica. Exp - 05',
+    enTitle: '',
+    author: 'Tsukasa Tomioka.',
+    visual: {
+      ...sampleVisual,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-007',
+    title: 'Fabrica. Exp - 07',
+    enTitle: '',
+    author: 'Tsukasa Tomioka.',
+    visual: {
+      ...sampleVisual,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-008',
+    title: 'Fabrica. Exp - 08',
+    enTitle: '',
+    author: 'Tsukasa Tomioka.',
+    visual: {
+      ...sampleVisual,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-006',
+    title: 'Fabrica. Exp - 06',
+    enTitle: '',
+    author: 'Tsukasa Tomioka.',
+    visual: {
+      ...sampleVisual,
+      altText: '',
+    }
+  },
+];
