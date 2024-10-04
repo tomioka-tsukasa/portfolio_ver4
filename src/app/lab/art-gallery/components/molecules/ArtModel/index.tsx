c17720c92cc6337b1fd4574dbfff08e03f3ca4ab
tomioka-tsukasa <tsukasa.creator@gmail.com>
2024-10-05 07:35:21 +0900

    add: ArtModelのscrollTrigger実装

--- src/app/lab/art-gallery/components/molecules/ArtModel/index.tsx
+++ src/app/lab/art-gallery/components/molecules/ArtModel/index.tsx
@@ -1,17 +1,55 @@
+import { Dispatch, useEffect, useState } from "react"
 import NamePlate from "../../atoms/NamePlate"
 import styles from "./_index.module.scss"
 import Image from "next/image"
 
 type Props = {
-  artModel: Cms.ArtModel
+  artModel: Cms.ArtModel,
+  scrollObserve?: boolean
+}
+
+const getBodyLeft = () => {
+  return innerHeight * (390 / 844)
+}
+
+const observe: (
+  e: HTMLElement
+) => boolean = (
+  e
+) => {
+  const rect = e?.getBoundingClientRect()
+  if (!rect || !e) return false
+  return rect.x - getBodyLeft() < document.body.clientWidth / 1.8
+}
+
+const scrollTrigger = (
+  artModel: Cms.ArtModel,
+  setView: Dispatch<boolean>
+) => {
+  function animation() {
+    const e = document.querySelector(`#${artModel.slug}`) as HTMLElement
+    setView(observe(e))
+    requestAnimationFrame(animation) 
+  }
+  requestAnimationFrame(animation)
 }
 
 export default function ArtModel({
-  artModel
+  artModel,
+  scrollObserve = false
 }: Props ) {
+  const [isView, setView] = useState<boolean>(true)
+  useEffect(() => {
+    setTimeout(() => {
+      scrollTrigger(
+        artModel,
+        setView
+      )
+    }, 1000);
+  }, [artModel, isView, setView])
   return <>
     <div className={styles.root}>
-      <div className={styles.image}>
+      <div className={`${styles.image} ${isView ? styles.isView : ''}`}>
         <Image
           src={artModel.visual.src}
           alt={artModel.visual.altText}
@@ -20,7 +58,7 @@ export default function ArtModel({
           priority
         />
       </div>
-      <div className={styles.plate}>
+      <div className={`${styles.plate} ${isView ? styles.isView : ''}`}>
         <NamePlate title={artModel.title} author={artModel.author} />
       </div>
     </div>
