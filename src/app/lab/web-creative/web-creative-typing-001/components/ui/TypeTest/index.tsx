a47ed883fee075983c09a7c41154b6d763a4f3b8
tomioka-tsukasa <tsukasa.creator@gmail.com>
2024-10-20 03:48:27 +0900

    add: typingLookahead実装（課題有）

--- src/app/lab/web-creative/web-creative-typing-001/components/ui/TypeTest/index.tsx
+++ src/app/lab/web-creative/web-creative-typing-001/components/ui/TypeTest/index.tsx
@@ -5,6 +5,7 @@ import styles from "./_index.module.scss"
 import { Typing } from "@/lib/utils/typing"
 import types from "../../../store/types.json"
 import { typingBasic } from "@/lib/utils/typing/modules/typingBasic"
+import { typingLookahead } from "@/lib/utils/typing/modules/typingLookahead"
 
 export default function TypeTest() {
   useEffect(() => {
@@ -17,31 +18,18 @@ export default function TypeTest() {
     }
     sample01.exec(
       'sample01',
-      typingBasic(
+      typingLookahead(
         types.hello,
         document.querySelector<HTMLElement>('[data-typing-id="sample01"]'),
       ).typeFunc,
+      () => endFunc()
     )
-    setTimeout(() => {
-      sample01.stop()
-    }, 500);
-    setTimeout(() => {
-      sample02.exec(
-        'sample02',
-        typingBasic(
-          types.hello,
-          document.querySelector<HTMLElement>('[data-typing-id="sample02"]'),
-        ).typeFunc,
-        () => endFunc()
-      ) 
-    }, 1000);
   })
   return <>
     <div className={styles.root}>
       <div className={styles.status} data-typing-id="end"></div>
       <div className={styles.samples}>
         <span className={styles.type} data-typing="target" data-typing-id="sample01"></span>
-        <span className={styles.type} data-typing="target" data-typing-id="sample02"></span>
       </div>
     </div>
   </>
