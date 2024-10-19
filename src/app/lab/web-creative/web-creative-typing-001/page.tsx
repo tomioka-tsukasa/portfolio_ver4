b2a50418ba107acf96a68dfa1be4b442e959d042
tomioka-tsukasa <tsukasa.creator@gmail.com>
2024-10-20 00:30:28 +0900

    add: typingクラスとtypingBasic実装

---
+++ src/app/lab/web-creative/web-creative-typing-001/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./_index.module.scss"
+import TypeTest from "./components/ui/TypeTest"
+
+export default function WebCreativeTyping() {
+  return <>
+    <div className={styles.root}>
+      <TypeTest />
+    </div>
+  </>
+}
